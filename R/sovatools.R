0ee5f4dff3a7e276562dd3889569f9aad4f134ea
Kelly Sovacool <kellysovacool@gmail.com>
2022-07-08 12:57:33 -0400

    Update pkg docs

---
+++ R/sovatools.R
@@ -0,0 +1,10 @@
+#' sovatools: Sovacool's Tools
+#'
+#' A collection of useful functions and example code that doesn't
+#' have an obvious home anywhere else. See the
+#' [website](http://sovacool.dev/sovatools)
+#' for more information, documentation, and examples.
+#'
+#' @docType package
+#' @name sovatools
+NULL
