bc3020b7e8dd73840185c263263c1e43d263ce42
Kelly Sovacool <kellysovacool@gmail.com>
2022-07-08 12:45:42 -0400

    Create theme_sovatools()

---
+++ R/ggplot.R
@@ -0,0 +1,24 @@
+#' sovatools theme for ggplot
+#'
+#' Uses `ggplot2::theme_bw()` and removes margins.
+#'
+#' @return list of ggproto objects
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mtcars) +
+#'   aes(x = mpg, y = wt, color = cyl) +
+#'   geom_point()  +
+#'   theme_sovatools()
+theme_sovatools <- function() {
+    abort_packages_not_installed('ggplot2')
+    return(list(
+        ggplot2::theme_bw(),
+        ggplot2::theme(
+            legend.margin = ggplot2::margin(0, 0, 0, 0, unit = "pt"),
+            legend.box.margin = ggplot2::margin(0, 0, 0, 0, unit = "pt"),
+            plot.margin = ggplot2::margin(0, 0, 0, 0, unit = "pt")
+        ))
+    )
+}
