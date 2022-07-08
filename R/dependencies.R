1ce1017b887b61fccae568fa310fc5a148cbc503
Kelly Sovacool <kellysovacool@gmail.com>
2022-07-08 12:45:20 -0400

    Create abort_packages_not_installed()

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,3 +1,31 @@
+#' Throw an error if required packages are not installed.
+#'
+#' Reports which packages need to be installed and the parent function name.
+#' See \url{https://stackoverflow.com/questions/15595478/how-to-get-the-name-of-the-calling-function-inside-the-called-routine}
+#' This is only intended to be used inside a function. It will error otherwise.
+#'
+#' Adapted from [mikropml](abort_packages_not_installed)
+#' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
+#'
+#' @examples
+#' abort_packages_not_installed("base")
+#' \dontrun{
+#' abort_packages_not_installed("not-a-package-name", "caret", "dplyr", "non_package")
+#' }
+abort_packages_not_installed <- function(...) {
+  package_status <- sapply(c(...), requireNamespace, quietly = TRUE)
+  parent_fcn_name <- sub("\\(.*$", "\\(\\)", deparse(sys.calls()[[sys.nframe() - 1]]))
+  packages_not_installed <- Filter(isFALSE, package_status)
+  if (length(packages_not_installed) > 0) {
+    msg <- paste0(
+      "The following package(s) are required for `", parent_fcn_name,
+      "` but are not installed: \n  ",
+      paste0(names(packages_not_installed), collapse = ", ")
+    )
+    stop(msg)
+  }
+}
 
 #' Get package dependencies as a tibble
 #'
@@ -9,15 +37,16 @@
 #' @examples
 #' get_deps_tbl('mikropml')
 get_deps_tbl <- function(package) {
+    abort_packages_not_installed(c('purrr', 'tidyr'))
     # https://stackoverflow.com/questions/14645363/listing-r-package-dependencies-without-installing-packages
-    mikropml_deps <- tools::package_dependencies(packages = package,
-                                                 recursive = TRUE)[[package]] %>%
+    pkg_deps <- tools::package_dependencies(packages = package,
+                                            recursive = TRUE)[[package]] %>%
         sort()
-    all_deps <- tools::package_dependencies(packages = mikropml_deps)
-    deps_tbl <- tibble::tibble(
+    all_deps <- tools::package_dependencies(packages = pkg_deps)
+    deps_tbl <- dplyr::tibble(
         Package = names(all_deps),
-        data = purrr::map(all_deps, tibble::as_tibble)
+        data = purrr::map(all_deps, dplyr::as_tibble)
     ) %>%
         tidyr::unnest(data)
-    deps_tbl %>% dplyr::filter(value == 'gtools')
+    return(deps_tbl)
 }
