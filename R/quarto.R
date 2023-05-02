1e42f5f20b98d2f26fa6ace513bc6fd7c6fb62c8
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-02 16:28:06 -0400

    Create helper functions for authoring with quarto

---
+++ R/quarto.R
@@ -0,0 +1,10 @@
+get_height_inches <- function(width_pixels, height_pixels, width_inches = 6.5) {
+    return(height_pixels / width_pixels * width_inches)
+}
+get_image_dims <- function(image_path, width_inches = 6.5) {
+    img_info <- image_info(image_read(image_path))
+    height_in <- get_height_inches(img_info$width, img_info$height,
+                                   width_inches = width_inches) %>%
+        round(.,1)
+    return(glue('("{image_path}"){{width="{width_inches}in" height="{height_in}in"}}'))
+}
