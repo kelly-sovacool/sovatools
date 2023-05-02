b9d9ec525121fc2e337d02aed482229e5d0c2055
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-02 16:53:23 -0400

    Don't re-print image path

--- R/quarto.R
+++ R/quarto.R
@@ -9,11 +9,11 @@ get_height_inches <- function(width_pixels, height_pixels, width_inches = 6.5) {
 #' @export
 #' @author Kelly Sovacool \email{sovacool@@umich.edu}
 get_image_dims <- function(image_path, width_inches = 6.5) {
-    img_info <- magick::image_info(here::here(magick::image_read(strip_leading_slash(image_path))))
+    img_info <- magick::image_info(magick::image_read(image_path))
     height_in <- get_height_inches(img_info$width, img_info$height,
                                    width_inches = width_inches) %>%
         round(.,1)
-    return(glue::glue('("{image_path}"){{width="{width_inches}in" height="{height_in}in"}}'))
+    return(glue::glue('{{width="{width_inches}in" height="{height_in}in"}}'))
 }
 
 #' Strip the leading slash from a string
