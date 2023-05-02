0cbaafcf618460eb279d0d2f9688f965838e31b4
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-02 16:32:40 -0400

    document

--- R/quarto.R
+++ R/quarto.R
@@ -1,6 +1,13 @@
+#' Get image height in inches given dimensions in pixels and width in inches
+#' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
 get_height_inches <- function(width_pixels, height_pixels, width_inches = 6.5) {
     return(height_pixels / width_pixels * width_inches)
 }
+
+#' Get image dimensions in inches from a file path
+#' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
 get_image_dims <- function(image_path, width_inches = 6.5) {
     img_info <- magick::image_info(magick::image_read(image_path))
     height_in <- get_height_inches(img_info$width, img_info$height,
