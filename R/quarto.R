cbf4f9c28eb23b404dbf2b53c0f0ee9f555f833c
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-02 16:29:33 -0400

    Use magick pkg

--- R/quarto.R
+++ R/quarto.R
@@ -2,7 +2,7 @@ get_height_inches <- function(width_pixels, height_pixels, width_inches = 6.5) {
     return(height_pixels / width_pixels * width_inches)
 }
 get_image_dims <- function(image_path, width_inches = 6.5) {
-    img_info <- image_info(image_read(image_path))
+    img_info <- magick::image_info(magick::image_read(image_path))
     height_in <- get_height_inches(img_info$width, img_info$height,
                                    width_inches = width_inches) %>%
         round(.,1)
