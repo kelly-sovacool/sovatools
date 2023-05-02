229b43c66e1dd22c2499b13e07e3412c67ad0d98
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-02 16:37:43 -0400

    Create strip_leading_slash()

--- R/quarto.R
+++ R/quarto.R
@@ -9,9 +9,17 @@ get_height_inches <- function(width_pixels, height_pixels, width_inches = 6.5) {
 #' @export
 #' @author Kelly Sovacool \email{sovacool@@umich.edu}
 get_image_dims <- function(image_path, width_inches = 6.5) {
-    img_info <- magick::image_info(magick::image_read(image_path))
+    img_info <- magick::image_info(magick::image_read(strip_leading_slash(image_path)))
     height_in <- get_height_inches(img_info$width, img_info$height,
                                    width_inches = width_inches) %>%
         round(.,1)
     return(glue::glue('("{image_path}"){{width="{width_inches}in" height="{height_in}in"}}'))
+}
+
+#' Strip the leading slash from a string
+#'
+#' @export
+#' @author Kelly Sovacool \email{sovacool@@umich.edu}
+strip_leading_slash <- function(x) {
+    return(stringr::str_remove(x, "^/"))
 }
